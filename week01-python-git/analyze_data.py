6d482e438827a512bcc918085b3cfeaa1a8573fe
naufal <naufal.nazaruddin@finexusgroup.com>
2025-05-04 15:14:16 +0700

    Refactor : add load_data and summarize_data functions

--- week01-python-git/analyze_data.py
+++ week01-python-git/analyze_data.py
@@ -1,12 +1,32 @@
+# #day2
+# import pandas as pd
+
+# # load the dataset
+# df = pd.read_csv("iris.csv")
+
+# # show first 5 rows
+# print("First 5 rows : ")
+# print(df.head(5))
+
+# # summary statistics
+# print("\nSummary statistics : ")
+# print(df.describe())
+
+#day3
 import pandas as pd
 
-# load the dataset
-df = pd.read_csv("iris.csv")
+def load_data(file_path):
+    return pd.read_csv(file_path)
 
-# show first 5 rows
-print("First 5 rows : ")
-print(df.head(5))
+def summarize_data(df):
+    print("\n2. First 5 rows : ")
+    print(df.head(5))
+    print("\n2. Summary Statistics : ")
+    print(df.describe())
 
-# summary statistics
-print("\nSummary statistics : ")
-print(df.describe())
+def main():
+    df = load_data("iris.csv")
+    summarize_data(df)
+
+if __name__ == "__main__":
+    main()
